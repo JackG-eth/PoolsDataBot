ee95e7fa8f517b85252caa7b088fb6af00340fa2
JackTracerSpore <jack.hawkins@mycelium.ventures>
2022-05-30 20:09:22 +0100

    added logic to pull new markets

--- RebalanceData.py
+++ RebalanceData.py
@@ -1,6 +1,6 @@
-
-from ast import Break
+from itertools import chain
 import json
+from multiprocessing import pool
 from pyparsing import empty
 import requests
 import pandas as pd
@@ -10,7 +10,6 @@ import openpyxl
 import xlsxwriter
 from openpyxl import load_workbook
 from os.path import exists
-
 
 def write_to_excel(tokenAddress):
     request = requests.get("https://api.tracer.finance/poolsv2/upkeeps?network=42161&poolAddress=" + tokenAddress)
@@ -54,7 +53,6 @@ def check_exists(excel_file ,df):
         return False
 
 def add_value(excel_file ,df):
-    print("added twice")
     rows = df.values.tolist()
     workbook = load_workbook(excel_file)
     sheet = workbook['Time Series Data']
@@ -63,11 +61,55 @@ def add_value(excel_file ,df):
     workbook.save(excel_file)
     workbook.close()
 
+def get_pool_list():
+    pools_file = 'data/pools.xlsx'
+    if exists(pools_file) == False:
+        workbook = xlsxwriter.Workbook(pools_file)
+        worksheet = workbook.add_worksheet("pools")
+        worksheet.write('A1', 'Address')  
+        workbook.close()
+
+    request = requests.get("https://api.tracer.finance/poolsv2/poolList?network=42161&list=verified")
+    request_text = request.text
+    JSON = json.loads(request_text)
+    list = []
+    for pool in JSON['pools']:
+        list.append(pool['address'])
+
+    workbook = load_workbook(pools_file)
+    idx= workbook.sheetnames.index('pools')
+    ws = workbook.get_sheet_by_name('pools')
+    workbook.remove(ws)
+    
+
+    workbook.create_sheet('pools', idx)
+    sheet = workbook['pools']
+    sheet.insert_rows(1)
+
+    # Create the pandas DataFrame
+    df = pd.DataFrame(list, columns = ['Address'])
+    rows = df.values.tolist()
+    for row in rows:
+        sheet.append(row)
+    workbook.save(pools_file)
+    workbook.close()
+
+    
+def schedule_pools():
+    existingPools = pd.read_excel('data/pools.xlsx', engine='openpyxl' ,sheet_name='pools')
+    existingPools = existingPools.dropna()
+    rows = list(chain.from_iterable(existingPools.values.tolist()))
+    for row in rows:
+        write_to_excel(row)
+        time.sleep(10)
+
+        
 
 if __name__ == "__main__":
-    schedule.every(3).seconds.do(lambda: write_to_excel('0x3c16b9efe5e4fc0ec3963f17c64a3dcbf7269207'))
-    schedule.every(3).seconds.do(lambda: write_to_excel('0x6d3fb4aa7ddca8cbc88f7ba94b36ba83ff6ba234'))
+    schedule.every(1).day.do(get_pool_list)
+    schedule.every(5).minutes.do(schedule_pools)
     while True:
         schedule.run_pending()
-   
+        
+
 
